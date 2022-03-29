67d69e539f37b2007da14aed1fb89a02e79f05d4
yunseo <redtastes@naver.com>
2022-03-29 12:18:48 +0900

    hello

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -3,11 +3,8 @@ package basic;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("연습");
- 	
-		
-		
 		System.out.println("안녕하세요");
-		
+		System.out.println("ddd");
 		System.out.println("날씨가 좋아요");
 	
 	}
