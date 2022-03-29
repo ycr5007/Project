db2145259301af7706ed4ac6e7048e7c74c82b62
chaeyoungshim <cyshim7@gmail.com>
2022-03-29 12:39:00 +0900

    채영

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -3,7 +3,7 @@ package basic;
 public class Hello {
 	public static void main(String[] args) {
 
-		System.out.println("연습");
+		System.out.println("덮어쓰기");
 		System.out.println("안녕하세요");
 		System.out.println("ddd");
 		System.out.println("날씨가 좋아요");
--- src/basic/WonniesGreeting.java
+++ src/basic/WonniesGreeting.java
@@ -5,7 +5,7 @@ public class WonniesGreeting {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요 팀원 여러분 반갑습니다");
 		
-
+		System.out.println("aaa");
 	}
 
 }
