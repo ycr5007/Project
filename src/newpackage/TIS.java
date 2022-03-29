7ca8cd6d7ef185bfd6921f27ee2fe107797cd193
yangchanryeol <ycr5007@gmail.com>
2022-03-29 12:42:54 +0900

    충돌 테스트

--- src/newpackage/TIS.java
+++ src/newpackage/TIS.java
@@ -3,5 +3,11 @@ package newpackage;
 public class TIS {
 	public static void main(String[] args) {
 		System.out.println("이건 ! 소스다 !");
+		
+		so();
+	}
+	
+	public static void so(	) {
+		System.out.println("이건 소스일까 ?");
 	}
 }
