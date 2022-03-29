6edc2e214d379f1090260085ee61567dc063569c
hayeong <hayeong0624@gmail.com>
2022-03-29 12:13:24 +0900

    하영

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -2,16 +2,7 @@ package basic;
 
 public class Hello {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello Java");
 		System.out.println("배고파요");
-=======
-		
-		
-		System.out.println("안녕하세요");
-		
-		System.out.println("날씨가 좋아요");
-	
->>>>>>> branch 'master' of https://github.com/ycr5007/Project.git
 	}
 }
